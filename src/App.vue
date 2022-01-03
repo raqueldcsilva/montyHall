cfa52f92d774eadcf7225ba216d8b208a792b969
Raqueldcsilva <raquelshdy@gmail.com>
2022-01-03 15:11:15 -0300

    formulario e lógica do jogo concluído

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
     <div id="app">
-        <h1>Problema de Monty Hall</h1>
-        <Gift />
+        <h1>Monty Hall</h1>
+        <div class="form">
+            <div v-if="!started">
+                <label for="portsAmount">Quantas portas? </label>
+                <input type="text" id="portsAmount" size="3" v-model.number="portsAmount">
+            </div>
+
+            <div v-if="!started">
+                <label for="selectedPort">Qual a porta é premiada? </label>
+                <input type="text" id="selectedPort" size="3" v-model.number="selectedPort">
+            </div>
+
+            <button v-if="!started" @click="started = true">Iniciar</button>
+            <button v-if="started" @click="started = false">Reiniciar</button>
+        </div>
+
+        <div class="doors" v-if="started">
+            <div v-for="i in portsAmount" :key="i">
+                <Door :hasGift="i === selectedPort" :number="i" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import Gift from './components/Gift'
+import Door from './components/Door'
 
 export default {
     name: 'App',
-    components: { Gift }
+    components: { Door },
+    data: function() {
+        return {
+            started: false,
+            portsAmount: 3,
+            selectedPort: null
+        }
+    }
 }
 </script>
 
